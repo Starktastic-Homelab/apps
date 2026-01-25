47e70f0751263a0850a34c88f5d14361287a49bd
Ben Faingold <benfaingold@microsoft.com>
2026-01-25 15:00:34 +0200

    Separate dyff into a file

---
+++ scripts/dyff-wrapper.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+
+# Wrapper to pass arguments ($@) from kubectl to dyff
+# This avoids kubectl's aggressive sanitization of environment variables
+exec dyff between \
+  --omit-header \
+  --set-exit-code \
+  --ignore-order-changes \
+  --exclude "metadata.generation" \
+  --exclude "metadata.resourceVersion" \
+  --exclude "metadata.managedFields" \
+  --exclude "metadata.uid" \
+  --exclude "metadata.creationTimestamp" \
+  --exclude "metadata.annotations.kubectl.kubernetes.io/last-applied-configuration" \
+  --exclude "metadata.annotations.argocd.argoproj.io/tracking-id" \
+  --exclude "status" \
+  --exclude "spec.source.targetRevision" \
+  "$@"

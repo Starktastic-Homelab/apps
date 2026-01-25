cea5e3616834a111f52ec3b1307efb8ea50e132e
Ben Faingold <benfaingold@microsoft.com>
2026-01-25 15:07:07 +0200

    Try to fix exclusions

--- scripts/dyff-wrapper.sh
+++ scripts/dyff-wrapper.sh
@@ -5,14 +5,13 @@
 exec dyff between \
   --omit-header \
   --set-exit-code \
-  --ignore-order-changes \
   --exclude "metadata.generation" \
   --exclude "metadata.resourceVersion" \
   --exclude "metadata.managedFields" \
   --exclude "metadata.uid" \
   --exclude "metadata.creationTimestamp" \
-  --exclude "metadata.annotations.kubectl.kubernetes.io/last-applied-configuration" \
-  --exclude "metadata.annotations.argocd.argoproj.io/tracking-id" \
+  --exclude 'metadata.annotations."kubectl.kubernetes.io/last-applied-configuration"' \
+  --exclude 'metadata.annotations."argocd.argoproj.io/tracking-id"' \
   --exclude "status" \
   --exclude "spec.source.targetRevision" \
   "$@"

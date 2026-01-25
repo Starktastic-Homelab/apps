ba8777aedd23e395a2f4d76fde93f20c47bfc18b
Ben Faingold <benfaingold@microsoft.com>
2026-01-25 15:15:50 +0200

    This should work

--- scripts/dyff-wrapper.sh
+++ scripts/dyff-wrapper.sh
@@ -5,13 +5,13 @@
 exec dyff between \
   --omit-header \
   --set-exit-code \
-  --exclude "metadata.generation" \
-  --exclude "metadata.resourceVersion" \
-  --exclude "metadata.managedFields" \
-  --exclude "metadata.uid" \
-  --exclude "metadata.creationTimestamp" \
-  --exclude "metadata.annotations.kubectl.kubernetes.io/last-applied-configuration" \
-  --exclude "metadata.annotations.argocd.argoproj.io/tracking-id" \
-  --exclude "status" \
-  --exclude "spec.source.targetRevision" \
+  --exclude "/metadata/generation" \
+  --exclude "/metadata/resourceVersion" \
+  --exclude "/metadata/managedFields" \
+  --exclude "/metadata/uid" \
+  --exclude "/metadata/creationTimestamp" \
+  --exclude "/metadata/annotations/kubectl.kubernetes.io/last-applied-configuration" \
+  --exclude "/metadata/annotations/argocd.argoproj.io/tracking-id" \
+  --exclude "/status" \
+  --exclude "/spec/source/targetRevision" \
   "$@"
